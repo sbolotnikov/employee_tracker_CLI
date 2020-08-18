3c13459f8c1515e60625688512d59dcfcabb9425
Sergey Bolotnikov <sbolotnikov@gmail.com>
2020-08-18 03:03:08 -0400

    finalised View all employees

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+const { Table } = require('console-table-printer');
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -15,22 +16,104 @@ var connection = mysql.createConnection({
     database: "employees_db"
 });
 
-connection.connect(function (err) {
-    if (err) throw err;
-    runAppChoice();
-});
+const allEmployeesList = () => {
+    var employees = [];
+    var roles = [];
+    var condition = '';
 
-function runAppChoice() {
-    inquirer
-        .prompt({
-            name: "action",
-            type: "rawlist",
+    var query = "SELECT manager_id FROM employee GROUP BY (manager_id);";
+    connection.query(query, function (err, res) {
+        if (err) throw err;
+        res.forEach(idItem => {
+            condition += ` OR (id=${idItem.manager_id})`
+        });
+        condition = condition.substr(4, condition.length - 4);
+        var query1 = "CREATE TABLE manager (SELECT id AS manager_id, CONCAT(COALESCE(first_name, ''),' ', COALESCE(last_name, ''))  AS m_name FROM employee WHERE " + condition + ");";
+        connection.query(query1, function (err, res) {
+            if (err) throw err;
+            var query = "INSERT INTO manager (manager_id, M_NAME) VALUE (0,'None')";
+            connection.query(query, function (err, res) {
+                if (err) throw err;
+                var query = "SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, departments.d_name, manager.m_name FROM employee JOIN role USING(role_id) JOIN departments USING(department_id) JOIN manager USING(manager_id) ORDER BY employee.id;";
+                connection.query(query, function (err, res) {
+                    // console.log(err);
+                    if (err) throw err;
+                    //Create a table
+                    const p = new Table({
+                        columns: [
+                            { name: 'index', alignment: 'left' },// color: 'blue'}, //with alignment and color
+                            { name: 'Name', alignment: 'right' },// color: 'white'},
+                            { name: 'Position', alignment: 'right' },// color: 'yellow'},
+                            { name: 'Salary', alignment: 'right' },// color: 'red'},
+                            { name: 'Department', alignment: 'right' },// color: 'blue'},
+                            { name: 'Manager', alignment: 'right' },// color: 'green'},
+                        ],
+                    });
+            
+                    //add rows with color
+                    for (let i = 0; i < res.length; i++) {
+                        p.addRow({
+                            index: res[i].id, Name: res[i].first_name + " " +
+                                res[i].last_name, Position: res[i].title, Salary: res[i].salary, Department: res[i].d_name, Manager: res[i].m_name
+                        }, { color: (i % 2) ? 'white' : 'blue' });
+                    }
+                    console.log('');
+                    p.printTable();
+                    try{
+                        var query1 = "DROP TABLE IF EXISTS manager;";
+                        connection.query(query1, function (err, res) {
+                            if (err) throw err;  
+                            runAppChoice();              
+                        });
+                    }catch(e){return}    
+                }) 
+                
+            });
+        });
+    });
+};
+const allByManager = () => {
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+const allByDepartment = () => {
+
+
+
+
+
+
+
+
+
+
+
+
+}
+const exitProgram = () => {
+    connection.end();
+    process.exit(0);
+}
+const runAppChoice = () => {
+    return inquirer.prompt([
+        {
             message: "What would you like to do?",
+            name: "query",
+            type: "list",
             choices: [
-                "View All employees",
-                "View All employees by department",
-                // "View employees by manager",
-                // "Add employee",
+                 // "Add employee",
                 // "Remove employee",
                 // "Update employee roles",
                 // "Update employee manager",
@@ -39,120 +122,32 @@ function runAppChoice() {
                 // "Update employee roles",
                 // "Update employee managers",
                 // "Delete departments, roles, and employees",
-                // "Find artists with a top song and top album in the same year"
+                {
+                    name: "View All employees",
+                    value: allEmployeesList
+                },
+                {
+                    name: "View All employees by manager",
+                    value: allByManager
+                },
+                {
+                    name: "View All employees by department",
+                    value: allByDepartment
+                },
+                {
+                    name: "EXIT",
+                    value: exitProgram
+                }
+                
             ]
-        })
-        .then(function (answer) {
-            switch (answer.action) {
-                case "View All employees":
-                    allEmployeesList();
-                    break;
-
-                case "Find all artists who appear more than once":
-                    multiSearch();
-                    break;
-
-                case "Find data within a specific range":
-                    rangeSearch();
-                    break;
-
-                case "Search for a specific song":
-                    songSearch();
-                    break;
-
-                case "Find artists with a top song and top album in the same year":
-                    songAndAlbumSearch();
-                    break;
-            }
-        });
-}
-
-function allEmployeesList() {
-    var employees = [];
-    var roles = [];
-    var condition = '';
-
-//    try {
-//     var query1 = "DROP TABLE IF EXISTS manager;";
-//     connection.query(query1, function (err, res) {
-//         console.log(err)
-//     });
-//    }catch(e){
-
-//    }
-
-
-
-    // SELECT manager_id, CONCAT(COALESCE(first_name, ''),' ', COALESCE(last_name, '')) AS m_name FROM employee WHERE (manager_id!='NULL') GROUP BY (manager_id);
-    var query = "SELECT manager_id FROM employee GROUP BY (manager_id);";
-    connection.query(query, function (err, res) {
-        console.log(err);
-        res.forEach(idItem => {
-            condition += ` OR (id=${idItem.manager_id})`
-        });
-
-        condition = condition.substr(4, condition.length - 4);
-        // var query1 = "CREATE TABLE manager(SELECT id, CONCAT(COALESCE(first_name, ''),' ', COALESCE(last_name, '')) FROM employee WHERE " + condition + ";)";
-        var query1 = "CREATE TABLE manager (SELECT id AS manager_id, CONCAT(COALESCE(first_name, ''),' ', COALESCE(last_name, ''))  AS m_name FROM employee WHERE " + condition + ");";
-        console.log(query1)
-        connection.query(query1, function (err, res) {
-            console.log(res);
-            console.log(err);
-        });
-    });
-    // var query = "INSERRT INTO manager SET m_name SELECT CONCAT(COALESCE(first_name, ''),' ', COALESCE(last_name, '')) FROM employee WHERE id=manager_id;";
-    // // CONCAT(COALESCE(first_name, ''),' ', COALESCE(last_name, '')) AS m_name
-    // connection.query(query, function (err, res) {
-    //     console.log(err);
-    // });
-    var query = "INSERT INTO manager (manager_id, M_NAME) VALUE (0,'None')";
-    connection.query(query, function (err, res) {
-        console.log(err);
-    });
-    var query2 = "SELECT * FROM manager";
-    connection.query(query2, function (err, res) {
-        console.log(res)
-    });
-
-
-    var query = "SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, departments.d_name, manager.m_name FROM employee JOIN role USING(role_id) JOIN departments USING(department_id) JOIN manager USING(manager_id) ORDER BY employee.id;";
-    connection.query(query, function (err, res) {
-        console.log(err);
-        // console.log(res)
-
-        for (let i = 0; i < res.length; i++) {
-            console.table([res[i].id , res[i].first_name + " " +
-                res[i].last_name , res[i].title, res[i].salary, res[i].d_name, res[i].m_name]);
         }
+    ])
+    .then( response => {
+        response.query();
     });
-   
-
-   try {
-    var query1 = "DROP TABLE IF EXISTS manager;";
-    connection.query(query1, function (err, res) {
-        console.log(err)
-    });
-   }catch(e){
-
-   }
-
-
-
-
-
-    // var query2 = "SELECT role_id, title, salary, department_id FROM role";
-    // connection.query(query2, function (err, res) {
-    //     roles = res;
-    //     console.log(res)
-    // });
-    // console.log("Second one");
-    // console.log(employees);
-    // console.log(employees[0].last_name + " || " + employees[0].first_name+" "+ employees[0].id)
-
+}
+connection.connect( err => {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
     runAppChoice();
-};
-
-
-// + roles[employees[i].role_id].title+" || " +
-//         res1[employees[i].role_id].salary+" || " + employees[employees[i].manager_id].first_name
-//         +" "+employees[employees[i].manager_id].last_name+" || "
+});
